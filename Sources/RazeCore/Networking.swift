2cbe1d7e793d8b736fd3dd4f042bd9d88be442b3
Francisco Ochoa <francisco.ochoa@mercadolibre.com>
2020-06-01 09:18:05 -0300

    Added second Networking API for sending data

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -8,8 +8,16 @@ import Foundation
 
 protocol NetworkSession {
     func get (from url: URL, completionHandler: @escaping (Data?, Error?) -> Void)
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?)-> Void)
 }
 extension URLSession:NetworkSession {
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) {
+        let task = dataTask(with: request){ data, _, error in
+            completionHandler(data,error)
+        }
+        task.resume()
+    }
+    
     func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
         let task = dataTask(with: url){ data, _, error in
             completionHandler(data,error)
@@ -42,7 +50,32 @@ extension RazeCore {
                 }
                 
             }
-           
+            
+            /// Calls to the live internet to send data to specific location
+            /// - Parameters:
+            ///   - url: The location you wish to send data to
+            ///   - body: The object you wish to send over networl
+            ///   - completationHandler: Returns a result object wich signifies the status of the request
+            public func sendData<I: Codable>(to url: URL, body: I,
+                                 completationHandler: @escaping(NetworkResult<Data>) -> Void ){
+                var request = URLRequest(url: url)
+                do {
+                   let httpBody =  try JSONEncoder().encode(body)
+                    request.httpBody = httpBody
+                    session.post(with: request) { data, error in
+                        let result = data.map(NetworkResult<Data>.success) ??
+                        .failure(error)
+                        completationHandler(result)
+                        
+                    }
+                } catch let error {
+                    return completationHandler(.failure(error))
+                }
+                
+                
+                
+            }
+        
             public enum NetworkResult<Value> {
                 case success(Value)
                 case failure(Error?)
--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -13,9 +13,14 @@ class NetworkSessionMock: NetworkSession {
     func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
         completionHandler(data,error)
     }
-    
-    
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?)-> Void) {
+        completionHandler(data,error)
+    }
+}
 
+struct MockData: Codable,Equatable {
+    var id : Int
+    var name: String
 }
 final class RazeNetworkingTests: XCTestCase {
 
@@ -41,10 +46,33 @@ final class RazeNetworkingTests: XCTestCase {
         wait(for: [expectation], timeout: 5.0)
     }
    
-
+    func testSendDataCall(){
+        let session = NetworkSessionMock()
+        let manager = RazeCore.Networking.Manager()
+        let sampleObject = MockData(id: 1, name: "David")
+        let data = try? JSONEncoder().encode(sampleObject)
+        session.data = data
+        manager.session = session
+        let url = URL(fileURLWithPath: "url")
+        let expectation = XCTestExpectation(description: "send data")
+      
+        manager.sendData(to: url, body: sampleObject) { result in
+            expectation.fulfill()
+            switch result{
+            case .success(let returnedData):
+                let returnedObject = try? JSONDecoder().decode(MockData.self, from: returnedData)
+                XCTAssertEqual(returnedObject, sampleObject)
+                break
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
+        }
+          wait(for: [expectation], timeout: 5)
+    }
    
     static var allTests = [
-    ("testLoadDataCall", testLoadDataCall)
+    ("testLoadDataCall", testLoadDataCall),
+    ("testSendDataCall",testSendDataCall)
     ]
 
 }
